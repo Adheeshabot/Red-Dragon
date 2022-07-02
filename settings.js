1d69cd53798e113bc5b88fa147d6b995271109bf
Adheeshabot <108585582+Adheeshabot@users.noreply.github.com>
2022-07-02 22:21:32 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //════════════(00)═══════════════════════════════════════//
 //
-//          RED DRAGON v5.0
+//          A.D.S. EDITOR v5.0
 //
 //════════════(00)════════════════//
 
@@ -22,24 +22,24 @@ global.APIKeys = {
 global.alivemsg = `My name is RED DRAGON🔥
 My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = [''] //Owner number in aive msg
+global.premium = [''] //Owner Number info
+global.ownernomer = ' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON' //Bot Name
+global.ownername = '😎ADHEESHA' //Owner Name
+global.botname = ' ☬ADS༒EDITOR' //Bot Name
 
-global.button = '🤭Youtube📍' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://youtu.be/E5C-lrl_g9I' //Change Your Button's URL
+global.button = '🤭📍' //Costomize A Button Name In Alive Message
+global.btnurl = '' //Change Your Button's URL
 
-global.region = 'India, South India, Kerala' //Country Region
+global.region = 'Sri Lanka' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = '☬ADS༒EDITOR࿐' //Bot Pacage Name
+global.author = '😎ADHEESHA❯❯' //Author Name
 global.prefa = ['','!','.','😂','❤','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor In Menu!
-global.footer = '©_Kumuthu.' //In list menu
+global.footer = '©_A.D.S.EDITOR.' //In list menu
 global.chatbotpre = 'true'  // Chat bot true/false
 
 
